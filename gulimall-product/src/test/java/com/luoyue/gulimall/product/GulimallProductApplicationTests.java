ea9f2ff583080ac0406be425a8e9915270ce1181
Lyue <L1316756108@163.com>
2022-08-23 11:56:51 +0800

    xiangmu测试

---
+++ gulimall-product/src/test/java/com/luoyue/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoyue.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
